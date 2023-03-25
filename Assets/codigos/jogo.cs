a18d832780e897a9e921f277deb34e017ff58b88
Gabriel Chaves Alencar <c.a.gabriel2011@hotmail.com>
2023-03-24 21:12:31 -0300

    281

--- Assets/codigos/jogo.cs
+++ Assets/codigos/jogo.cs
@@ -26,7 +26,7 @@ public class jogo : MonoBehaviour
     
     // Start is called before the first frame update
     private void Awake() {
-       //GameManager.CurrentLevel = 3;
+       GameManager.CurrentLevel = 6;
        MainMenu.num_perso = 5;
        if(!NetworkControler.online){
             GameObject temp = Instantiate(player);
